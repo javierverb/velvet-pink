65fc4ae0149dee564b11c0d5f2279ef54376885a
javierverb <javier2mil8@gmail.com>
2022-01-03 23:34:08 -0300

    added whatsapp:// to check browser android

--- js/app.js
+++ js/app.js
@@ -8,16 +8,18 @@ window.onscroll = () => {
 };
 
 const hydrateURL = (text) => {
-  const phone = "+5492942560668";
-  return `https://wa.me/${phone}?text=${text}`;
+  const phone = "5492942560668";
+  /* checks  first if the browser is mobile */
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  return isMobile
+    ? `whatsapp://${phone}?text=${text}`
+    : `https://web.whatsapp.com/send?phone=${phone}&text=${text}`;
 };
 
 function sendMessage(kindOfService) {
   const products = ["uñas", "depilación"];
   const baseMessage = products.includes(kindOfService)
-    ? `
-      Hola! me gustaría agendar un turno para ${kindOfService}! :) cuál es el precio y las fechas disponibles?
-    `
+    ? `Hola! me gustaría agendar un turno para ${kindOfService}! :) cuál es el precio y las fechas disponibles?`
     : kindOfService;
   const url = hydrateURL(baseMessage);
   window.open(url, "_blank");
