e2f7cbff2da3287100728247ee2452cce530f42a
javierverb <javier2mil8@gmail.com>
2022-01-03 23:02:03 -0300

    added app.js, removed unused files

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const scrollTop = document.querySelector("#return-to-top");
+window.onscroll = () => {
+  if (window.scrollY > 300) {
+    scrollTop.style.visibility = "visible";
+  } else {
+    scrollTop.style.visibility = "hidden";
+  }
+};
+
+const hydrateURL = (text) => {
+  const phone = "+5492942560668";
+  return `https://api.whatsapp.com/send?phone=${phone}&text=${text}`;
+};
+
+function sendMessage(kindOfService) {
+  const baseMessage = `
+    Hola!, me gustaría agendar un turno para ${kindOfService}! :) cuál es el precio y las fechas disponibles?
+  `;
+  const url = hydrateURL(baseMessage);
+  window.open(url, "_blank");
+}
